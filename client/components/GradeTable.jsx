8153ae4bb852fb91ca693c7af1f9ade5522701c9
Jake Hassler <jphyr4@mail.missouri.edu>
2020-01-17 20:20:52 +0000

    default message for no entries

--- client/components/GradeTable.jsx
+++ client/components/GradeTable.jsx
@@ -4,25 +4,51 @@ import Grade from './grade';
 function GradeTable(props) {
 
   const tempArr = props.gradeSent.map((element, index) => <Grade key={index} sending={element} deleteProp={props.deleteProp}/>);
-  return (
-    <div className="table-responsive tablePadding">
-      <table className="table table-hover table-striped tbody text-center">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Course</th>
-            <th scope="col">Grade</th>
-            <th className="hideOperation" scope="col">Ops</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tempArr}
-        </tbody>
 
-      </table>
-    </div>
-  );
+  if (tempArr.length === 0) {
+    return (
+      <div className="table-responsive tablePadding">
+        <table className="table table-hover table-striped tbody text-center">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Course</th>
+              <th scope="col">Grade</th>
+              <th className="hideOperation" scope="col">Ops</th>
+            </tr>
+          </thead>
 
+          <thead>
+            <tr>
+              <th colSpan="4">No entries available</th>
+
+            </tr>
+          </thead>
+
+        </table>
+      </div>
+    );
+  } else {
+
+    return (
+      <div className="table-responsive tablePadding">
+        <table className="table table-hover table-striped tbody text-center">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Course</th>
+              <th scope="col">Grade</th>
+              <th className="hideOperation" scope="col">Ops</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tempArr}
+          </tbody>
+
+        </table>
+      </div>
+    );
+  }
 }
 
 export default GradeTable;
